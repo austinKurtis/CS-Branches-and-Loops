23d17ed69bd29818e7d4f1d14e3eca5097a043b5
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-01-31 14:01:41 -0600

    Make decisions using the if statement

--- BranchesLoops/Program.cs
+++ BranchesLoops/Program.cs
@@ -6,7 +6,13 @@ namespace BranchesLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 5;
+            //int b = 6;
+            int b = 3;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10");
+
+
         }
     }
 }
